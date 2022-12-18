423fef20f1e3e8a5da5541b1ee239d21796315d4
Emil Noren <stratosfloor@hotmail.com>
2022-12-18 15:41:46 +0100

    Set up parkingapp project

--- src/main/java/edu/lernia/parking/ParkingApplication.java
+++ src/main/java/edu/lernia/parking/ParkingApplication.java
@@ -1,7 +1,9 @@
 package edu.lernia.parking;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ParkingApplication {
@@ -9,5 +11,12 @@ public class ParkingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingApplication.class, args);
 	}
+
+    @Bean
+    CommandLineRunner init() {
+        return args -> {
+
+        };
+    }
 
 }
