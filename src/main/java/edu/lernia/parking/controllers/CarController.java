78ed8e45aeccc6c8a079138705f07c15f965e1aa
Emil Noren <stratosfloor@hotmail.com>
2023-01-02 19:56:38 +0100

    Set up car controller

---
+++ src/main/java/edu/lernia/parking/controllers/CarController.java
@@ -0,0 +1,19 @@
+package edu.lernia.parking.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.lernia.parking.entity.Car;
+import edu.lernia.parking.repository.CarRepository;
+
+@RestController
+@RequestMapping("/api/cars")
+public class CarController {
+  CarRepository carRepository;
+
+  public CarController(CarRepository carRepository) {
+    this.carRepository = carRepository;
+  }
+
+}
