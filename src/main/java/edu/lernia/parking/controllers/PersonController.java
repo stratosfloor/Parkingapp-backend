9e192823243a20a338a15cc12cc10d43763ba18e
Emil Noren <stratosfloor@hotmail.com>
2023-01-02 19:47:39 +0100

    Add endpoint to add new person

--- src/main/java/edu/lernia/parking/controllers/PersonController.java
+++ src/main/java/edu/lernia/parking/controllers/PersonController.java
@@ -7,8 +7,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import edu.lernia.parking.entity.Person;
 import edu.lernia.parking.repository.CarRepository;
@@ -38,5 +42,18 @@ public class PersonController {
     return ResponseEntity.notFound().build();
   }
   
+  @PostMapping()
+  public ResponseEntity<Person> createNewPerson(@RequestBody Person person) {
+    var p = personRepository.save(person);
+    return ResponseEntity
+      .created(ServletUriComponentsBuilder
+      .fromCurrentRequest()
+      .path("/{id}")
+      .buildAndExpand(p.getId())
+      .toUri())
+      .body(p);
+      
+      
+  }
 
 }
