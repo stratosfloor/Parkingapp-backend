22b8a7b24e666be0b47357096d33c3f50fa75cd2
Emil Noren <stratosfloor@hotmail.com>
2023-01-02 17:33:26 +0100

    Refactor

--- src/main/java/edu/lernia/parking/ParkingApplication.java
+++ src/main/java/edu/lernia/parking/ParkingApplication.java
@@ -1,13 +1,8 @@
 package edu.lernia.parking;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import edu.lernia.parking.entity.Person;
-import edu.lernia.parking.repository.CarRepository;
-import edu.lernia.parking.repository.PersonRepository;
 
 @SpringBootApplication
 public class ParkingApplication {
