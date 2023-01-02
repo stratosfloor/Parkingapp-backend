79740a9139ff74e70ff2c87ef6c900c961c6ede1
Emil Noren <stratosfloor@hotmail.com>
2023-01-02 17:44:47 +0100

    Add endpoint api/persons/{id}

--- src/main/java/edu/lernia/parking/controllers/PersonController.java
+++ src/main/java/edu/lernia/parking/controllers/PersonController.java
@@ -1,8 +1,12 @@
 package edu.lernia.parking.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +27,15 @@ public class PersonController {
   @GetMapping()
   public Iterable<Person> getAllPersons() {
     return personRepository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Optional<Person>> getPersonById(@PathVariable("id") Long id) {
+    var person = personRepository.findById(id);
+    if(person.isPresent()) { 
+      return ResponseEntity.ok().body(person);
+    }
+    return ResponseEntity.notFound().build();
   }
   
 
