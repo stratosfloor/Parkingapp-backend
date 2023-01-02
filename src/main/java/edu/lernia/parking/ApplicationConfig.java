7e50912de0e8d7b7a125a80f9f173831a0f00f86
Emil Noren <stratosfloor@hotmail.com>
2023-01-02 17:08:19 +0100

    Add application config commandlinerunnder do create some objects int db

---
+++ src/main/java/edu/lernia/parking/ApplicationConfig.java
@@ -0,0 +1,44 @@
+package edu.lernia.parking;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import edu.lernia.parking.entity.Person;
+import edu.lernia.parking.entity.Car;
+import edu.lernia.parking.repository.CarRepository;
+import edu.lernia.parking.repository.PersonRepository;
+
+@Configuration
+public class ApplicationConfig {
+  
+  
+  @Bean
+  CommandLineRunner init(PersonRepository personRepository, CarRepository carRepository) {
+      return args -> {
+
+        Person p1 = new Person();
+        p1.setFirstName("Canilla");
+        p1.setLastName("Kiwson");
+        personRepository.save(p1);
+
+        Person p2 = new Person();
+        p2.setFirstName("Enil");
+        p2.setLastName("Kiwson");
+        personRepository.save(p2);
+
+        Car car1 = new Car();
+        car1.setRegnr("LMA714");
+        car1.setOwner(p1);
+        carRepository.save(car1);
+
+        Car car2 = new Car();
+        car2.setRegnr("ABC123");
+        carRepository.save(car2);
+
+        Car car3 = new Car();
+        car3.setRegnr("HPB857");
+        carRepository.save(car3);
+      };
+  }
+}
--- src/main/java/edu/lernia/parking/ParkingApplication.java
+++ src/main/java/edu/lernia/parking/ParkingApplication.java
@@ -5,18 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import edu.lernia.parking.entity.Person;
+import edu.lernia.parking.repository.CarRepository;
+import edu.lernia.parking.repository.PersonRepository;
+
 @SpringBootApplication
 public class ParkingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingApplication.class, args);
 	}
-
-    @Bean
-    CommandLineRunner init() {
-        return args -> {
-
-        };
-    }
 
 }
