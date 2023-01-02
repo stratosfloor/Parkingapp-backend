34cd8d2c499474dd17c5db16ecbb283e29c71db8
Emil Noren <stratosfloor@hotmail.com>
2023-01-02 20:22:36 +0100

    Add endpoint to get a car from id

--- src/main/java/edu/lernia/parking/controllers/CarController.java
+++ src/main/java/edu/lernia/parking/controllers/CarController.java
@@ -1,8 +1,15 @@
 package edu.lernia.parking.controllers;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import edu.lernia.parking.entity.Car;
 import edu.lernia.parking.repository.CarRepository;
@@ -20,4 +27,15 @@ public class CarController {
   public Iterable<Car> getAllCars() {
     return carRepository.findAll();
   }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Optional<Car>> getCarById(@PathVariable("id") Long id) {
+    var car = carRepository.findById(id);
+    if(car.isPresent()) {
+      return ResponseEntity.ok().body(car);
+    }
+    return ResponseEntity.notFound().build();
+  }
+
+
 }
