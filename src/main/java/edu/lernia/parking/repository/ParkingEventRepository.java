f62d847a0c6dd001cb47f1fbed41434f366e5a50
Emil Noren <stratosfloor@hotmail.com>
2023-01-04 21:23:11 +0100

    Change findAll to return List

--- src/main/java/edu/lernia/parking/repository/ParkingEventRepository.java
+++ src/main/java/edu/lernia/parking/repository/ParkingEventRepository.java
@@ -1,9 +1,14 @@
 package edu.lernia.parking.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import edu.lernia.parking.entity.ParkingEvent;
 
 public interface ParkingEventRepository extends CrudRepository<ParkingEvent, Long> {
+
+  @Override
+  List<ParkingEvent> findAll();
   
 }
