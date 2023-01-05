09c25d92ff75b1a82c766d85ca78aef4cb8640e2
Emil Noren <stratosfloor@hotmail.com>
2023-01-05 06:51:20 +0100

    Add endpoint to get one parkingevent with id

--- src/main/java/edu/lernia/parking/controllers/ParkingEventController.java
+++ src/main/java/edu/lernia/parking/controllers/ParkingEventController.java
@@ -1,8 +1,11 @@
 package edu.lernia.parking.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +26,15 @@ public class ParkingEventController {
   @GetMapping()
   public List<ParkingEvent> getAllParkingEvents() {
     return parkingEventRepository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Optional<ParkingEvent>> getParkingEventFromId(@PathVariable("id") Long id) {
+    var event = parkingEventRepository.findById(id);
+    if(event.isPresent()) {
+      return ResponseEntity.ok().body(event);
+    }
+    return ResponseEntity.notFound().build();
   }
   
 }
