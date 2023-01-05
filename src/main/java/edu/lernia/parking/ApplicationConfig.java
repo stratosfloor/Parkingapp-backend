e405f3be38cefaed7fe27dd9bc74de1c81f6970a
Emil Noren <stratosfloor@hotmail.com>
2023-01-05 12:01:53 +0100

    Add more entitys to db from start

--- src/main/java/edu/lernia/parking/ApplicationConfig.java
+++ src/main/java/edu/lernia/parking/ApplicationConfig.java
@@ -1,20 +1,40 @@
 package edu.lernia.parking;
 
+import org.geolatte.geom.G2D;
+import org.geolatte.geom.Point;
+import org.geolatte.geom.json.GeolatteGeomModule;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import edu.lernia.parking.entity.Person;
 import edu.lernia.parking.entity.Car;
+import edu.lernia.parking.entity.ParkingEvent;
+import edu.lernia.parking.entity.ParkingLot;
 import edu.lernia.parking.repository.CarRepository;
+import edu.lernia.parking.repository.ParkingEventRepository;
+import edu.lernia.parking.repository.ParkingLotRepository;
 import edu.lernia.parking.repository.PersonRepository;
+
+import static org.geolatte.geom.builder.DSL.*;
+import static org.geolatte.geom.crs.CoordinateReferenceSystems.WGS84;
 
 @Configuration
 public class ApplicationConfig {
+
+  @Bean
+  GeolatteGeomModule geolatteModule(){
+    return new GeolatteGeomModule();
+ }
+
   
   
   @Bean
-  CommandLineRunner init(PersonRepository personRepository, CarRepository carRepository) {
+  CommandLineRunner init(
+    PersonRepository personRepository, 
+    CarRepository carRepository, 
+    ParkingLotRepository parkingLotRepository, 
+    ParkingEventRepository parkingEventRepository) {
       return args -> {
 
         Person p1 = new Person();
@@ -41,6 +61,37 @@ public class ApplicationConfig {
         car3.setRegnr("HPB857");
         car3.setPerson(p2);
         carRepository.save(car3);
+
+        ParkingLot pl1 = new ParkingLot();
+        pl1.setName("Nanna");
+        Point<G2D> po1 = point(WGS84,g(123, 123));
+        pl1.setCordinates(po1);
+        parkingLotRepository.save(pl1);
+
+        ParkingLot pl2 = new ParkingLot();
+        pl2.setName("Örnen");
+        Point<G2D> po2 = point(WGS84,g(234, 234));
+        pl2.setCordinates(po2);
+        parkingLotRepository.save(pl2);
+
+        ParkingEvent pe1 = new ParkingEvent();
+        pe1.setPerson(p1);
+        pe1.setCar(car1);
+        pe1.setParkingLot(pl1);
+        parkingEventRepository.save(pe1);
+
+        ParkingEvent pe2 = new ParkingEvent();
+        pe2.setPerson(p1);
+        pe2.setCar(car2);
+        pe2.setParkingLot(pl1);
+        parkingEventRepository.save(pe2);
+
+        ParkingEvent pe3 = new ParkingEvent();
+        pe3.setPerson(p2);
+        pe3.setCar(car3);
+        pe3.setParkingLot(pl2);
+        parkingEventRepository.save(pe3);
+        
       };
   }
 }
